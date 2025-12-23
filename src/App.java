143cbc2463cf89c32ac55b079c8e68df8717550c
phongha <havanphong784@gmail.com>
2025-12-23 09:38:29 +0700

    remove file

--- src/App.java
+++ src/App.java
@@ -6,15 +6,3 @@ public class App {
         new UI.LoginFrame().setVisible(true);
     }
 }
-
-
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0 ; i < nums.length ; i++ ) {
-            for (int y = 0 ; y < nums.length ; y++ ) {
-                if (i + y == target) return new int[] {i,y};
-            }
-        }
-        return null;
-    }
-}
